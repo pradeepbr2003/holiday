724a12666e44ede98c64fb6500c415e6b9c6f42f
pradeepbr2003 <pradeepbr2003@gmail.com>
2025-05-21 13:09:17 +0530

    refactored holiday

--- src/main/java/job/search/HolidayInt.java
+++ src/main/java/job/search/HolidayInt.java
@@ -9,18 +9,16 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
-import static job.search.EnumMain.*;
+import static job.search.helper.GeneralEnum.*;
+import static job.search.helper.RegExEnum.DATE_PATTERN;
+import static job.search.helper.RegExEnum.DATE_REG_EX;
 
-public interface IMain {
-    static void extractHoliday(Map<Date, Set<String>> mhMap, Map<Date, Set<String>> fhMap) {
+public interface HolidayInt {
+    static void extractHoliday(Map<Date, Set<String>> holidayMap) {
         try {
             List<String> contents = Files.readAllLines(Paths.get(HOLIDAYS_LOG.value()));
             contents.forEach(content -> {
-                if (content.contains(MANDATORY_HOLIDAY.value())) {
-                    extractHoliday(content, mhMap);
-                } else {
-                    extractHoliday(content, fhMap);
-                }
+                extractHoliday(content, holidayMap);
             });
         } catch (IOException e) {
             System.out.printf("%n %s %n", e.getMessage());
@@ -29,16 +27,15 @@ public interface IMain {
 
     static void extractHoliday(String content, Map<Date, Set<String>> holidayMap) {
         Matcher matcher = DATE_REG_EX.pattern().matcher(content);
-        Matcher reasonMatcher = HOLIDAY_REASON_REG_EX.pattern().matcher(content);
-        if (matcher.find() && reasonMatcher.find()) {
+        if (matcher.find()) {
             String dateStr = matcher.group();
-            String reasonStr = reasonMatcher.group();
             try {
-                Date date = DATE_PATTERN_STR.sdf().parse(dateStr);
+                Date date = DATE_PATTERN.formatter().parse(dateStr);
+                String holidayReason = content.replaceAll(DATE_REG_EX.value(), EMPTY_STR.value());
                 if (holidayMap.containsKey(date)) {
-                    holidayMap.get(date).add(reasonStr);
+                    holidayMap.get(date).add(holidayReason);
                 } else {
-                    holidayMap.put(date, new HashSet<>(List.of(reasonStr)));
+                    holidayMap.put(date, new HashSet<>(List.of(holidayReason)));
                 }
             } catch (ParseException e) {
                 System.out.printf("%n Exception : %s %n", e.getMessage());
@@ -46,19 +43,22 @@ public interface IMain {
         }
     }
 
-    static boolean isWeenEnd(Map.Entry<Date, Set<String>> e) {
-        for (String str : e.getValue()) {
-            if (str.contains(SATURDAY.value()) || str.contains(SUNDAY.value())) return true;
+    static boolean isWeekDayAndType(Map.Entry<Date, Set<String>> e, String holidayType) {
+        boolean flag = e.getValue().stream().noneMatch(str -> (str.contains(SATURDAY.value()) || str.contains(SUNDAY.value()) || !str.contains(holidayType)));
+        if (flag) {
+            e.setValue(e.getValue().stream().map(str -> str.replaceAll(holidayType, EMPTY_STR.value()))
+                    .collect(Collectors.toSet()));
+            return true;
         }
         return false;
     }
 
-    static void printHolidays(Map<Date, Set<String>> holidayMap, String message) {
+    static void printHolidays(Map<Date, Set<String>> holidayMap, String holidayType) {
         System.out.println("-----------------------------------------------------");
-        System.out.printf("%n %s %n", message);
+        System.out.printf("%n %s %n", holidayType);
         System.out.println("-----------------------------------------------------");
         Map<Integer, List<Map.Entry<Date, Set<String>>>> holidays = holidayMap.entrySet().stream()
-                .filter(e -> !isWeenEnd(e))
+                .filter(e -> isWeekDayAndType(e, holidayType))
                 .collect(Collectors.groupingBy(e -> e.getKey().getMonth()));
         holidays.forEach((k, v) -> {
             System.out.printf("%n Month - %s %n", Month.values()[k].name());
--- src/main/java/job/search/helper/RegExEnum.java
+++ src/main/java/job/search/helper/RegExEnum.java
@@ -1,25 +1,19 @@
-package job.search;
+package job.search.helper;
 
 import java.text.SimpleDateFormat;
 import java.util.regex.Pattern;
 
-public enum EnumMain {
-    HOLIDAYS_LOG("holidays.log"),
-    MANDATORY_HOLIDAYS("Mandatory Holidays"),
-    FLOATING_HOLIDAYS("Floating Holidays"),
-    MANDATORY_HOLIDAY("Mandatory holiday"),
+public enum RegExEnum {
+    DATE_PATTERN("dd-MMM-yyyy"),
     DATE_REG_EX("\\d+-\\w{3}-\\d{4}"),
-    DATE_PATTERN_STR("dd-MMM-yyyy"),
     HOLIDAY_REASON_REG_EX("(for){1}\\D+"),
-    SATURDAY("Saturday"),
-    SUNDAY("Sunday"),
     LOCATION_REG_EX("\\s(in){1}\\s\\w+");
 
     private String value;
     private Pattern pattern;
     private SimpleDateFormat sdf;
 
-    EnumMain(String value) {
+    RegExEnum(String value) {
         this.value = value;
     }
 
@@ -34,7 +28,7 @@ public enum EnumMain {
         return this.pattern;
     }
 
-    public SimpleDateFormat sdf() {
+    public SimpleDateFormat formatter() {
         if (this.sdf == null) {
             this.sdf = new SimpleDateFormat(value());
         }
